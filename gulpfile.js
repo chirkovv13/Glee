69b2f7f5f2d0f9ddd4a5aaef4961724df0a21d57
Chirkovv Artem <patyomkin2011@mail.ru>
2021-06-08 13:44:32 +0300

    fixed galp error

--- gulpfile.js
+++ gulpfile.js
@@ -82,7 +82,7 @@ function cleanDist() {
 
 function watching() {
   watch(['app/scss/**/*.scss'], styles);
-  watch(['app/js/**/*.js', '!app/js/MediaDeviceInfo.min.js'], scripts);
+  watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
   watch(['app/**/*.html']).on('change', browserSync.reload);
 }
 
